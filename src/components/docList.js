32a34b097b7a4ff5c6478c5db336944dbe701c7c
栋栋晓 <2243980491@qq.com>
2016-08-17 08:56:09 +0800

    u

--- src/components/docList.js
+++ src/components/docList.js
@@ -3,19 +3,32 @@ import docItem from './docItem.js';
 
 import '../App.css';
 
+class RepeatLi extends Component{
+	render(){
+		return (
+			<ol>
+				{
+					this.props.children.map((child) => {return <li>{child}</li>})
+				}
+			</ol>
+		);
+	}
+}
+
+
 class docList extends Component {
 	render(){
-		
-
 		return (
-		  <div className="docList">
-          	<docItem /> 
-          </div>
+				<RepeatLi className="docList">
+					<docItem /> 
+				</RepeatLi>
+
 		)
 	}
 }
 
 export default docList;
+
 
 
 
